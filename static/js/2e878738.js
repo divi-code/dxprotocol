971e0521a17239ea8414584da5cd11a21fa4a76b
Femi Akin <feolou2021@gmail.com>
2023-08-23 21:36:53 +0100

    first commit

---
+++ static/js/2e878738.js
@@ -0,0 +1,254 @@
+import { _ as n, c as t } from "./35bc27a3.js";
+import {
+  j as e,
+  G as i,
+  q as o,
+  H as s,
+  I as a,
+  y as r,
+  O as h,
+  x as u,
+  F as c,
+  a1 as l,
+  u as d,
+  J as g,
+} from "./5bdea638.js";
+import "./ee7ce8fe.js";
+import "./1e98e625.js";
+import "./9b9cf7db.js";
+import "./2b425f2d.js";
+import "./c229d341.js";
+import "./9737f36b.js";
+const m = {
+    "zh-CN": {
+      1: {
+        question: "什么是DxProtocol金库？",
+        answer:
+          "DxProtocol金库是交易者的交易对手方。金库的回报完全基于用户的交易行为。作为DxProtocol金库的流动性提供者，用户可以简单地将USDT存入金库并分享平台收入的30%。",
+      },
+      2: {
+        question: "关于从金库获得的收益是否有任何锁定限制？",
+        answer:
+          "存款和奖励没有锁定期限。在将资金存入金库后，需要至少24小时才能发起提款。",
+      },
+      3: {
+        question: "金库的回报来自哪里？",
+        answer:
+          "根据过去7天的数据计算出的年化回报率。其中包括资金费用、借款费用和交易费用，但不包括实现的PNL对金库净值的实时影响。收入基于自动再投资计算。",
+      },
+      4: {
+        question: "有哪些措施来确保金库中持有的用户资金安全？",
+        answer:
+          "用户完全掌握自己的资产。团队无法在任何时间访问或管理用户资金。支持平台的智能合约已由领先的安全公司进行了安全漏洞审计。",
+      },
+      5: {
+        question: "金库目前支持哪些资产或加密货币？",
+        answer: "目前仅支持USDT。",
+      },
+    },
+    "en-US": {
+      1: {
+        question: "What is DxProtocol Vault?",
+        answer:
+          "DxProtocol vault is the counter party of traders. The return of vault is all based on users' trading behaviors. As a liquidity provider of DxProtocol vault, users could simply stake USDT into the vault and share 30% of platform revenue.",
+      },
+      2: {
+        question:
+          "Are there any lock-up restrictions on the returns earned from the vault? ",
+        answer:
+          "No lock-up period for your deposit and rewards. After depositing funds into the vault, it requires at least 24 hours before initiating a withdrawal.",
+      },
+      3: {
+        question: "Where do the returns from the vault come from?",
+        answer:
+          "The calculated annualized rate of return by accounting for the data of the past 7 days. This includes funding fees, borrowing fees, and trading fees, but does not include the real-time impact of realized PNL on the net value of the Vault.The income is calculated based on automatic reinvestment.",
+      },
+      4: {
+        question:
+          "What measures are in place to secure user funds held in the vault?",
+        answer:
+          "Users retain full control of their assets. The team has no ability to access or manage user funds at any time. Smart contracts powering the platform have been audited for security vulnerabilities by leading security firms.",
+      },
+      5: {
+        question:
+          "What assets or cryptocurrencies does the vault currently support?",
+        answer: "USDT will be the only supported asset at this moment.",
+      },
+    },
+    "ja-JP": {
+      1: {
+        question: "DxProtocolの金庫とは何ですか？",
+        answer:
+          "DxProtocolの金庫はトレーダーの相手側です。金庫のリターンはすべてユーザーの取引行動に基づいています。DxProtocolの金庫の流動性プロバイダーとして、ユーザーは単純にUSDTを金庫にステーキングしてプラットフォームの収益の30%を共有できます。",
+      },
+      2: {
+        question: "金庫から得たリターンにはロックアップの制約はありますか？",
+        answer:
+          "預金と報酬にはロックアップ期間はありません。金庫に資金を預け入れた後、少なくとも24時間経過する必要があります。",
+      },
+      3: {
+        question: "金庫からのリターンはどこから来るのですか？",
+        answer:
+          "過去7日間のデータを基に計算された年間リターン率です。これには資金調達手数料、借入手数料、取引手数料が含まれますが、金庫の純資産に対する実現PNLのリアルタイム影響は含まれません。収入は自動再投資に基づいて計算されます。",
+      },
+      4: {
+        question:
+          "金庫に保管されているユーザー資産の安全性を確保するための対策は何ですか？",
+        answer:
+          "ユーザーは自分の資産を完全にコントロールします。チームはいつでもユーザーの資金にアクセスしたり管理したりする能力を持っていません。プラットフォームを支えるスマートコントラクトは、主要なセキュリティ企業によってセキュリティの脆弱性を対象とした監査を受けています。",
+      },
+      5: {
+        question: "金庫は現在どの資産または暗号通貨をサポートしていますか？",
+        answer: "現時点ではUSDTのみがサポートされています。",
+      },
+    },
+    "ko-KR": {
+      1: {
+        question: "DxProtocol 보관소란 무엇인가요?",
+        answer:
+          "DxProtocol 보관소는 거래자의 대counter party입니다. 보관소의 수익은 모두 사용자의 거래 행동을 기반으로 합니다. DxProtocol 보관소의 유동성 공급자로서 사용자는 간단히 USDT를 보관소에 스테이킹하여 플랫폼 수익의 30%를 공유할 수 있습니다.",
+      },
+      2: {
+        question: "보관소에서 얻은 수익에 대한 잠금 제한이 있나요?",
+        answer:
+          "보증금과 보상에 대한 잠금 기간은 없습니다. 보관소에 자금을 예치한 후 출금을 시작하기 전에 최소 24시간이 소요됩니다.",
+      },
+      3: {
+        question: "보관소에서 나오는 수익은 어디에서 오는 건가요?",
+        answer:
+          "과거 7일의 데이터를 기반으로 계산한 연 평균 수익률입니다. 이에는 자금비용, 대출비용 및 거래 수수료가 포함되지만, 실현 PNL이 보관소 순가치에 미치는 실시간 영향은 포함되지 않습니다. 수입은 자동 재투자에 기반하여 계산됩니다.",
+      },
+      4: {
+        question:
+          "보관소에 보관된 사용자 자금을 보호하기 위해 어떤 조치가 있나요?",
+        answer:
+          "사용자는 자산을 완전히 제어합니다. 팀은 언제든지 사용자 자금에 액세스하거나 관리할 수 있는 능력이 없습니다. 플랫폼을 구동하는 스마트 계약은 주요 보안 회사에 의해 보안 취약점에 대한 감사를 받았습니다.",
+      },
+      5: {
+        question: "보관소는 현재 어떤 자산이나 암호화폐를 지원하나요?",
+        answer: "현재 USDT만 지원됩니다.",
+      },
+    },
+    "vi-VN": {
+      1: {
+        question: "DxProtocol Kho bạc là gì?",
+        answer:
+          "DxProtocol Kho bạc là đối tác của các nhà giao dịch. Lợi nhuận từ Kho bạc hoàn toàn dựa trên hành vi giao dịch của người dùng. Là nhà cung cấp thanh khoản của DxProtocol Kho bạc, người dùng có thể đơn giản là stake USDT vào Kho bạc và chia sẻ 30% doanh thu của nền tảng.",
+      },
+      2: {
+        question:
+          "Có bất kỳ hạn chế khóa cố định nào về lợi nhuận từ Kho bạc không?",
+        answer:
+          "Không có khoảng thời gian khóa cố định cho khoản tiền gửi và phần thưởng. Sau khi gửi tiền vào Kho bạc, cần ít nhất 24 giờ trước khi tiến hành rút tiền.",
+      },
+      3: {
+        question: "Lợi nhuận từ Kho bạc đến từ đâu?",
+        answer:
+          "Tỷ suất lợi nhuận hàng năm được tính toán bằng cách tích hợp dữ liệu của 7 ngày qua. Điều này bao gồm phí tài trợ, phí vay và phí giao dịch, nhưng không bao gồm tác động thời gian thực của PNL thực tế lên giá trị ròng của Kho bạc. Thu nhập được tính toán dựa trên tái đầu tư tự động.",
+      },
+      4: {
+        question:
+          "Có biện pháp nào để bảo đảm an toàn các khoản tiền gửi của người dùng trong Kho bạc?",
+        answer:
+          "Người dùng giữ quyền kiểm soát đầy đủ về tài sản của họ. Nhóm không có khả năng truy cập hoặc quản lý tài sản của người dùng vào bất kỳ thời điểm nào. Hợp đồng thông minh làm hoạt động của nền tảng đã được các công ty bảo mật hàng đầu kiểm tra lỗ hổng bảo mật.",
+      },
+      5: {
+        question: "Kho bạc hiện hỗ trợ tài sản hoặc tiền mã hóa nào?",
+        answer: "Hiện tại chỉ hỗ trợ USDT.",
+      },
+    },
+    "zh-TW": {
+      1: {
+        question: "什麼是DxProtocol金庫？",
+        answer:
+          "DxProtocol金庫是交易者的交易對手方。金庫的回報完全基於用戶的交易行為。作為DxProtocol金庫的流動性提供者，用戶可以簡單地將USDT存入金庫並分享平台收入的30%。",
+      },
+      2: {
+        question: "關於從金庫獲得的收益是否有任何鎖定限制？",
+        answer:
+          "存款和獎勵沒有鎖定期限。在將資金存入金庫後，需要至少24小時才能發起提款。",
+      },
+      3: {
+        question: "金庫的回報來自哪裡？",
+        answer:
+          "根據過去7天的數據計算出的年化回報率。其中包括資金費用、借款費用和交易費用，但不包括實現的PNL對金庫淨值的實時影響。收入基於自動再投資計算。",
+      },
+      4: {
+        question: "有哪些措施來確保金庫中持有的用戶資金安全？",
+        answer:
+          "用戶完全掌握自己的資產。團隊無法在任何時間訪問或管理用戶資金。支持平台的智能合約已由領先的安全公司進行了安全漏洞審計。",
+      },
+      5: {
+        question: "金庫目前支持哪些資產或加密貨幣？",
+        answer: "目前僅支持USDT。",
+      },
+    },
+  },
+  p = { style: { "font-size": "16px" } },
+  q = { class: "question-content" },
+  v = { class: "item" },
+  b = { key: 0, class: "question" },
+  w = { class: "answer" },
+  f = n(
+    {
+      __name: "Question",
+      props: { visible: { type: Boolean, default: !1 } },
+      emits: ["update:visible"],
+      setup(n, { emit: f }) {
+        const { locale: K } = t(),
+          y = e(() => m[K.value || "en-US"] || m["en-US"]),
+          k = () => {
+            f("update:visible", !1);
+          };
+        return (t, e) => {
+          const m = i("a-modal");
+          return (
+            o(),
+            s(
+              m,
+              {
+                visible: n.visible,
+                onCancel: k,
+                "modal-class": "kilo-question-modal",
+                "render-to-body": !0,
+                footer: !1,
+                width: "500px",
+                "title-align": "start",
+                "unmount-on-close": "",
+              },
+              {
+                title: a(() => [r("span", p, h(t.$t("vault.FAQ")), 1)]),
+                default: a(() => [
+                  r("div", q, [
+                    (o(!0),
+                    u(
+                      c,
+                      null,
+                      l(
+                        d(y),
+                        (n) => (
+                          o(),
+                          u("div", v, [
+                            n.question
+                              ? (o(), u("div", b, h(n.question), 1))
+                              : g("", !0),
+                            r("div", w, h(n.answer), 1),
+                          ])
+                        )
+                      ),
+                      256
+                    )),
+                  ]),
+                ]),
+                _: 1,
+              },
+              8,
+              ["visible"]
+            )
+          );
+        };
+      },
+    },
+    [["__scopeId", "data-v-6c98a883"]]
+  );
+export { f as default };
---
+++ static/js/56385387.js
@@ -0,0 +1,1 @@
+import{K as s,j as t}from"./35bc27a3.js";const i=s({url:t.pythKline,timeout:1e4}),r=s=>i.get("/v1/shims/tradingview/history",{params:{...s}});export{r as p};
